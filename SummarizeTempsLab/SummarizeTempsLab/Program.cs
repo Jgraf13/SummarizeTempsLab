247f793b959c9a4634d23aabb5eb3d857a46aec0
Jgraf13 <59981685+Jgraf13@users.noreply.github.com>
2020-02-11 16:43:40 -0600

    12
    new

--- SummarizeTempsLab/SummarizeTempsLab/Program.cs
+++ SummarizeTempsLab/SummarizeTempsLab/Program.cs
@@ -7,7 +7,74 @@ namespace SummarizeTempsLab
     {
         static void Main(string[] args)
         {
-            // temperature data is in temps.txt
+            string filename;
+
+            Console.WriteLine("Enter File Name");
+
+            filename = Console.ReadLine();
+
+            if (File.Exists(filename))
+            {
+                Console.WriteLine("File exists");
+
+                Console.WriteLine("Enter Threshold");
+
+                string input;
+                int Threshold;
+                input = Console.ReadLine();
+                Threshold = int.Parse(input);
+                int sumTemps = 0;
+                int numAbove = 0;
+                int numBelow = 0;
+                int tempCount = 0;
+
+
+                using (StreamReader sr = File.OpenText(filename))
+                {
+
+                    string line = sr.ReadLine();
+                    int temp;
+
+                    while (line != null)
+                    {
+                        temp = int.Parse(input);
+
+                        sumTemps += temp;
+
+                        tempCount += 1;
+
+                        if (temp >= Threshold)
+                        {
+                            numAbove += 1;
+
+                        }
+
+                        else
+                        {
+                            numBelow += 1;
+                        }
+
+                        line = sr.ReadLine();
+
+                    }
+
+
+                }
+
+                Console.WriteLine("Temperature above =" + numAbove);
+                Console.WriteLine("Temperature below =" + numBelow);
+                int average = sumTemps / tempCount;
+                Console.WriteLine("Average temp" + average);
+
+            }
+            else
+            {
+                Console.WriteLine("File does not exist");
+            }
+
         }
+        
+
     }
+
 }
